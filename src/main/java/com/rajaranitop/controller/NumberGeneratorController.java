eb41ac49db68b14d0766208c09e398168d68283a
Aditya Sharma <aditya.sharma.adbu@gmail.com>
2023-12-28 23:23:38 +0530

    fixing cors issue

--- src/main/java/com/rajaranitop/controller/NumberGeneratorController.java
+++ src/main/java/com/rajaranitop/controller/NumberGeneratorController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/lottery")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*",allowedHeaders = "*")
 public class NumberGeneratorController {
 
     @Autowired

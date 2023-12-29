2a96f76d831202f22586db3b57200409bf99b51e
Aditya Sharma <aditya.sharma.adbu@gmail.com>
2023-12-29 22:04:27 +0530

    fixing cors issue for post

--- src/main/java/com/rajaranitop/dto/LoginDTO.java
+++ src/main/java/com/rajaranitop/dto/LoginDTO.java
@@ -3,9 +3,11 @@ package com.rajaranitop.dto;
 import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoginDTO {
 
     private @NotNull String username;
